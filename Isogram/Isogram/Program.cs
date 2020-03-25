cfc8092224a2e9099573097dd99bf236efd3d42a
Charlie <creddin788@gmail.com>
2020-03-25 15:55:57 +0000

    Added 2 c# challenges

---
+++ Isogram/Isogram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Isogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            Console.WriteLine(IsIsogram(str1));
+        }
+
+        public static bool IsIsogram(string str)
+        {
+            str = str.ToLower();
+            int len = str.Length;
+
+            char[] arr = str.ToCharArray();
+
+            Array.Sort(arr);
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                    return false;
+            }
+            return true;
+        }
+
+    }
+
+}
+
+
